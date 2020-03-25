64d8b7e6e237c55823f67f520b3916749f8be61e
awkstep <awksjen@gmail.com>
2020-03-25 22:34:06 +0000

    fix file so git stuff was deleted out of it

--- models.py
+++ models.py
@@ -17,13 +17,9 @@ class Author(db.Model):
     author_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
     quotes = db.relationship('Quote', backref='author')
-<<<<<<< HEAD
-    #sources = db.relationship('Quote', secondary='sources')
-=======
->>>>>>> ec52212b70e2697f830d74f530f17c01174749eb
 
     def __repr__(self):
-        return f'<Author {self.id} | {self.name}>'
+        return f'<Author {self.author_id} | {self.name}>'
 
 
 class Quote(db.Model):
@@ -33,28 +29,18 @@ class Quote(db.Model):
     quote_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     aquote = db.Column(db.String(), nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('authors.author_id'))
-<<<<<<< HEAD
-    #sources = db.relationship('Author', secondary='sources')
-=======
->>>>>>> ec52212b70e2697f830d74f530f17c01174749eb
 
     def __repr__(self):
-        return f'<Quote {self.id} | {self.aquote} >'
+        return f'<Quote {self.quote_id} | {self.aquote} >'
 
-
-<<<<<<< HEAD
 # class Sources(db.Model):
-
 #     __tablename__ = 'sources'
-
 #     author_id = db.Column(db.Integer, db.ForeignKey('authors.author_id'))
 #     quote_id = db.Column(db.Integer, db.ForeignKey('quotes.quote_id'))
 #     source = db.Column(db.String())
 #     source_id = db.Column(db.Integer, primary_key=True)
 
 
-=======
->>>>>>> ec52212b70e2697f830d74f530f17c01174749eb
 def connect_to_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///database'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -65,19 +51,12 @@ def connect_to_db(app):
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
+
     from flask import Flask
-=======
-    from flask import Flask 
->>>>>>> ec52212b70e2697f830d74f530f17c01174749eb
     app = Flask(__name__)
 
     connect_to_db(app)
     db.drop_all()
     db.create_all()
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> ec52212b70e2697f830d74f530f17c01174749eb
     print('Connected to database, tables ready.')
