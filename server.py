b98098ded7f7699b891def12f6319c90086ab989
AwksJen <awksjen@gmail.com>
2020-03-31 20:24:07 -0500

    connect seed and views

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
+from seed import QUOTES
+from models import connect_to_db, db, Author, Quote
 from views import app
-from models import connect_to_db
 
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
+
+    from flask import Flask
+    app = Flask(__name__)
 
     connect_to_db(app)
 
